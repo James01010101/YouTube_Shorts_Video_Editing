00b585b0eb2bc5b8b385b8bb2c6f807699f22818
James01010101 <jamescoldwell73@gmail.com>
2024-04-19 16:17:29 +0930

    Added all main frontend files and config files

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Updated from 'contentBase' to 'static.directory'
+    },
+    compress: true,
+    port: 3000,
+    open: true
+  }
+};
