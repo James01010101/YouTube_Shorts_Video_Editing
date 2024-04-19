369b8878dc690fe284ff904d3c9b595e8c0df5dd
James01010101 <jamescoldwell73@gmail.com>
2024-04-19 18:29:36 +0930

    adding ability to use css files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']  // Add this line
       }
     ]
   },
