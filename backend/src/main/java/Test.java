c0a59a4ce08be4e1609633330f9c7fde81239e76
James <jamescoldwell73@gmail.com>
2024-04-30 23:59:07 +0930

    Working on Creating TextObjects and Rendering them to the screen

--- backend/src/main/java/Test.java
+++ backend/src/main/java/Test.java
@@ -5,26 +5,31 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.font.GlyphVector;
 
 import com.main.TextObject;
 
 public class Test {
     public static void main(String[] args) {
         // Create a BufferedImage with desired width and height
-        int width = 1080;
-        int height = 1920;
-        BufferedImage introImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        int[] screenSize = new int[]{1080, 1920};
+        BufferedImage introImage = new BufferedImage(screenSize[0], screenSize[1], BufferedImage.TYPE_INT_ARGB);
 
         // Get the Graphics2D object from the BufferedImage
         Graphics2D g2d = introImage.createGraphics();
 
+        // Improve rendering quality
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+
         // draw background
         g2d.setColor(Color.BLACK);
-        g2d.fillRect(0, 0, width, height);
+        g2d.fillRect(0, 0, screenSize[0], screenSize[1]);
 
 
-        TextObject quickText = new TextObject("Quick", "Arial", 50, new float[]{100.0f, 100.0f}, Color.BLUE);
-        TextObject howWellText = new TextObject("How Well Do You Know", "Arial", 50, new float[]{300.0f, 100.0f}, Color.RED);
+        TextObject quickText = new TextObject("Quick", "Arial", 100, new int[]{0, 150}, Color.YELLOW, 20, 900, -15);
+        TextObject howWellText = new TextObject("How Well Do You Know", "Arial", 100, new int[]{0, 300}, Color.BLUE, 20, 900, -15);
 
         // load in my background image
         BufferedImage backgroundImage = null;
@@ -36,12 +41,25 @@ public class Test {
         g2d.drawImage(backgroundImage, 0, 0, null);
 
 
-
-
-
         // Set the font and color of the text
-        g2d = quickText.ApplyGraphics(g2d);
-        g2d = howWellText.ApplyGraphics(g2d);
+        quickText.ApplyGraphics(g2d, screenSize);
+        howWellText.ApplyGraphics(g2d, screenSize);
+
+
+        /*
+        // Convert text to shape
+        GlyphVector gv = font.createGlyphVector(g2d.getFontRenderContext(), text);
+        Shape textShape = gv.getOutline(x, y);
+
+        // Draw the outline
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(thickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); // Set the outline thickness
+        g2d.draw(textShape);
+
+         // Draw the main text over the outline
+        g2d.setColor(Color.RED); // Main text color
+        g2d.drawString(text, x, y);
+        */
 
 
         // Dispose the Graphics2D object
--- backend/src/main/java/com/main/TextObject.java
+++ backend/src/main/java/com/main/TextObject.java
@@ -9,20 +9,26 @@ import java.io.IOException;
 // here ill create all of the text i need and allows me to modify it as i need
 
 public class TextObject {
-    private String text;
-    private String fontname;
-    private Font font;
-    private int size;
-    private float[] position;
-    private Color colour;
+    private String text; // text to write
+    private String fontname; // fonts name
+    private Font font; // actual font object
+    private int size; // size of the text
+    private int[] positionOffset; // this is an offset from center xy
+    private Color colour; // colour of the main text
+    private int thickness; // thickness of the black border around the text
+    private int maxCharWidth; // width in pixels before the text wraps onto a new line
+    private int interlineOffset; // offset between lines of text
 
-
-    public TextObject(String text, String font, int size, float[] position, Color colour) {
+    public TextObject(String text, String font, int size, int[] positionOffset, Color colour, int thickness, int maxCharWidth, int interlineOffset) {
         this.text = text;
         this.fontname = font;
         this.size = size;
-        this.position = position;
+        this.positionOffset = positionOffset; 
         this.colour = colour;
+        this.thickness = thickness;
+        this.maxCharWidth = maxCharWidth;
+        this.interlineOffset = interlineOffset;
+
 
         this.font = new Font(this.fontname, Font.PLAIN, this.size);
     }
@@ -44,23 +50,80 @@ public class TextObject {
         size = newSize;
     }
 
-    public float[] GetPosition() {
-        return position;
+    public int[] GetPositionOffset() {
+        return positionOffset;
     }
-    public void SetPosition(float[] newPosition) {
-        position = newPosition;
+    public void SetPositionOffset(int[] newPositionOffset) {
+        positionOffset = newPositionOffset;
     }
 
 
 
     // this will set the graphics render with all of the settings for this text
     // like colour and position and everything
-    public Graphics2D ApplyGraphics(Graphics2D g2d) {
-        g2d.setFont(font);
-        g2d.setColor(colour);
-        g2d.drawString(text, position[0], position[1]);
+    public void ApplyGraphics(Graphics2D g2d, int[] screenSize) {
+        /* PARAMS
+        g2d: Graphics2D object
+        text: The text to draw
+        font: The font to use
+        xOffset: The x offset from the center
+        yOffset: The y offset from the center
+        thickness: The thickness of the black border line
+        interlineOffset: The offset between lines
+        maxCharWidth: The max size before text is wrapped (in pixels) onto a new line
+        screenSize: The screen size
+        */
+        FontMetrics fm = g2d.getFontMetrics();
+        int lineHeight = fm.getHeight();
+        String[] words = text.split(" ");
+        StringBuilder line = new StringBuilder();
+        Shape textShape = null;
+        String textToPrint = "";
 
-        return g2d;
+        int centeredX = 0;
+        lineHeight += interlineOffset; // adjust the interline from what is chosen
+        g2d.setFont(font);
+
+        int yPosition = positionOffset[1];
+
+        for (String word : words) {
+            String nextLine = line.toString() + " " + word;
+            if (fm.stringWidth(nextLine.trim()) > maxCharWidth) {
+                textToPrint = line.toString().trim();
+                centeredX = ((screenSize[0] - fm.stringWidth(textToPrint)) / 2) + positionOffset[0];
+                textShape = font.createGlyphVector(g2d.getFontRenderContext(), textToPrint).getOutline(centeredX, yPosition);
+
+                // Draw the outline
+                g2d.setColor(Color.BLACK);
+                g2d.setStroke(new BasicStroke(thickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); // Set the outline thickness
+                g2d.draw(textShape);
+
+                g2d.setColor(colour);
+                g2d.drawString(textToPrint, centeredX, yPosition);
+
+
+                line = new StringBuilder(word + " ");
+                yPosition += lineHeight;
+            } else {
+                line.append(word).append(" ");
+            }
+        }
+
+        //TODO: #14 if text doesnt wrap it is incorrectly centered on x axis
+        if (line.length() > 0) {
+            textToPrint = line.toString().trim();
+            centeredX = ((screenSize[0] - fm.stringWidth(textToPrint)) / 2) + positionOffset[0];
+            textShape = font.createGlyphVector(g2d.getFontRenderContext(), textToPrint).getOutline(centeredX, yPosition);
+            
+            System.out.println(textToPrint + centeredX);
+
+            // Draw the outline
+            g2d.setColor(Color.BLACK);
+            g2d.setStroke(new BasicStroke(thickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); // Set the outline thickness
+            g2d.draw(textShape);
+
+            g2d.setColor(colour);
+            g2d.drawString(textToPrint, centeredX, yPosition);
+        }
     }
-    
 }
